3660abb693b67866ee6aa044fa368b7f2ff0ffba
iLegalJm <josemcz2211@gmail.com>
2023-07-18 11:08:08 -0500

    arreglando

--- Config/config.php
+++ Config/config.php
@@ -5,6 +5,6 @@ define('URL', 'http://localhost:8080');
 define('HOST', 'localhost');
 define('DB', 'neog');
 define('USER', 'root');
-define('PASSWORD', '');
+define('PASSWORD', '..MalTin2022');
 define('CHARSET', 'utf8mb4');
-define('PORT', '3306');
+define('PORT', '3377');
